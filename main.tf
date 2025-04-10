7df501afa6dec109e7fda93bcdd64bdf4da710c4
Martin Hrášek <martin.hrasek@drmax.eu>
2025-04-10 22:31:14 +0200

    Correction

--- main.tf
+++ main.tf
@@ -5,16 +5,16 @@
  */
 
 resource "azurerm_service_plan" "this" {
-  name                            = var.name
-  resource_group_name             = data.azurerm_resource_group.this.name
-  location                        = var.location != "" ? var.location : data.azurerm_resource_group.this.location
-  os_type                         = var.os_type
-  sku_name                        = var.sku_name
-  worker_count                    = var.worker_count == 0 ? null : var.worker_count
-  zone_balancing_enabled          = var.zone_balancing_enabled
-  per_site_scaling_enabled        = var.per_site_scaling_enabled
-  maximum_elastic_worker_count    = var.maximum_elastic_worker_count == 0 ? null : var.maximum_elastic_worker_count
-  premium_plan_auto_scale_enabled = var.premium_plan_auto_scale_enabled
+  name                         = var.name
+  resource_group_name          = data.azurerm_resource_group.this.name
+  location                     = var.location != "" ? var.location : data.azurerm_resource_group.this.location
+  os_type                      = var.os_type
+  sku_name                     = var.sku_name
+  worker_count                 = var.worker_count == 0 ? null : var.worker_count
+  zone_balancing_enabled       = var.zone_balancing_enabled
+  per_site_scaling_enabled     = var.per_site_scaling_enabled
+  maximum_elastic_worker_count = var.maximum_elastic_worker_count == 0 ? null : var.maximum_elastic_worker_count
+  //premium_plan_auto_scale_enabled = var.premium_plan_auto_scale_enabled
 
   tags = merge(local.metadata.tags, var.tags)
 
